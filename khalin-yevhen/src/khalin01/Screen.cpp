f2139fcde2e73d5c5845b498a40fd8b24561038b
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-12 17:00:01 +0300

    #1 khalin01: set logger for Screen class

--- khalin-yevhen/src/khalin01/Screen.cpp
+++ khalin-yevhen/src/khalin01/Screen.cpp
@@ -1,11 +1,14 @@
 #include "Screen.h"
+#include "Logger.h"
 
 Screen::Screen(Button btn): button(btn)
 {
+	Logger::getLogger().info("Screen()");
 }
 
 Screen::~Screen()
 {
+	Logger::getLogger().info("~Screen()");
 }
 
 void Screen::printButtonState(std::ostream &out)
