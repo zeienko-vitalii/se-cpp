a0ba296c6fa132a5a26dc671eb62fe97742d8f77
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-14 08:47:09 +0300

    #5 khalin01: added test method for showing objects life cycle

--- khalin-yevhen/src/khalin01/main.cpp
+++ khalin-yevhen/src/khalin01/main.cpp
@@ -6,11 +6,17 @@
 */
 
 #include "Screen.h"
+#include "Logger.h"
+
 using namespace std;
 
-void main() {
+inline void testObjLifeCycle() {
 	Button btn;
 	Screen scr(btn);
 	scr.printButtonState(cout);
+}
+
+void main() {
+	testObjLifeCycle();
 	system("pause");
 }
