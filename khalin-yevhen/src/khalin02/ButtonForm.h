f3e793e5b17e600fa44b831a3811ef51ab1ed54e
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-17 19:38:38 +0300

    #1 khalin02: created ButtonForm enum declaration

---
+++ khalin-yevhen/src/khalin02/ButtonForm.h
@@ -0,0 +1,17 @@
+/**
+* @file ButtonForm.h
+* @brief Contains a declaration of the Button forms
+* @author Khalin Yevhen
+* @version 0
+* @date 17.09.17
+*/
+
+#pragma once
+
+/**
+* @brief Represents button forms
+* @author Khalin Yevhen
+*/
+enum ButtonForm {
+	round, rectangular
+};
