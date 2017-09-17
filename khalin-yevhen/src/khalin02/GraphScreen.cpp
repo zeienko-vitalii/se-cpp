7d1e8c93f1b2b822560bf3581822884a370d6d87
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-17 22:42:29 +0300

    #1 khalin02: added GraphScreen class implementation

---
+++ khalin-yevhen/src/khalin02/GraphScreen.cpp
@@ -0,0 +1,33 @@
+/**
+* @file GraphScreen.cpp
+* @brief Contains GraphScreen class implementation
+* @author Khalin Yevhen
+* @version 0
+* @date 17.09.17
+*/
+
+#include "GraphScreen.h"
+
+GraphScreen::GraphScreen(const Button * btn) : btn(btn)
+{
+}
+
+void GraphScreen::printData(const Button * btn, std::ostream & os) const
+{
+	char* isPressed = btn->isRoundPressed() ? "true" : "false";
+	os << "Button is round and pressed: " << isPressed << std::endl;
+}
+
+void GraphScreen::setDataSource(const Button * btn)
+{
+	this->btn = btn;
+}
+
+const Button * GraphScreen::getDataSource()
+{
+	return btn;
+}
+
+GraphScreen::~GraphScreen()
+{
+}
