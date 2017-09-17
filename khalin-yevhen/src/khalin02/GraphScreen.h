43ea3564f385bde166007fea5354b80f6809ffe3
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-17 22:17:09 +0300

    #1 khalin02: created GraphScreen class declaration

---
+++ khalin-yevhen/src/khalin02/GraphScreen.h
@@ -0,0 +1,47 @@
+/**
+* @file GraphScreen.h
+* @brief Contains a declaration of the GraphScreen class
+* @author Khalin Yevhen
+* @version 0
+* @date 17.09.17
+*/
+
+#pragma once
+
+#include "Button.h"
+#include <iostream>
+
+/**
+* @brief Represents methods for printing data of the Button object
+* @author Khalin Yevhen
+*/
+class GraphScreen
+{
+public:
+
+	/**
+	* @param btn a pointer to an object that will be held
+	*/
+	GraphScreen(const Button *btn);
+
+	/**
+	* @param btn an object whose data will be printed into a stream
+	* @param os a stream for printing object data
+	*/
+	void printData(const Button *btn, std::ostream &os) const;
+
+	/**
+	* @param btn a source object
+	*/
+	void setDataSource(const Button *btn);
+
+	/**
+	* @return a button pointer
+	*/
+	const Button * getDataSource();
+
+	~GraphScreen();
+private:
+	const Button* btn;
+};
+
