29db7d430a61a9cad25fcd623d0e05fd7630ef2b
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-22 17:07:33 +0300

    #1 khalin03_tests: added entry point to application

---
+++ khalin-yevhen/src/khalin03_tests/main.cpp
@@ -0,0 +1,17 @@
+/**
+* @file main.cpp
+* @brief Contains the entry point to the console application
+* @author Khalin Yevhen
+* @version 0.0.1
+* @date 22.09.17
+*/
+
+#include <gtest\gtest.h>
+#include "TestClassInstances.h"
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	const int res = RUN_ALL_TESTS();
+	system("pause");
+	return res;
+}
