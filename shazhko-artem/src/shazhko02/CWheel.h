17e445f00f3d12255978efa8742f7be942f2e02b
Shazhko-Artem <31850295+Shazhko-Artem@users.noreply.github.com>
2017-09-22 14:38:22 +0300

    #2 shazhko02: Реализация класса CWheel

---
+++ shazhko-artem/src/shazhko02/CWheel.h
@@ -0,0 +1,57 @@
+/**
+* @file CWheel.h
+* @brief Содержит объявление класса  CWheel
+* @author Shazhko Artem
+* @version 0
+* @date 18.09.17
+*/
+#pragma once
+#include <string>
+/**
+* Перечисление единиц измерения
+*/
+enum EUnit
+{
+	EUNIT_CENTIMETERS, EUNIT_MILLIMETRES, EUNIT_INCHES
+};
+
+/**
+* Класс описывающий колесо
+*/
+class CWheel {
+public:
+	// Диаметр
+	double diameter;
+	// Шырина
+	double width;
+	/**
+	* Конструктор по-умолчанию
+	*/
+	CWheel();
+	/**
+	* Конструктор с тремя параметром
+	* @param _diameter Диаметр
+	* @param _width Шырина
+	* @param _unit Единицы измерения
+	*/
+	CWheel(double _diameter, double _width, EUnit _unit);
+	/**
+	* Конструктор копировнаия
+	* @param wheel копируемый объект
+	*/
+	CWheel(const CWheel *wheel);
+
+	/*
+	* Деструктор
+	*/
+	virtual ~CWheel();
+	/**
+	* Подсчет объема колеса
+	*/
+	double Volume() const;
+	EUnit GetUnit() const;
+	static std::string ConvertEUnitToString(const EUnit _unit);
+private:
+	// Единицы измерения
+	EUnit unit;
+};
--- shazhko-artem/src/shazhko02/Source.cpp
+++ shazhko-artem/src/shazhko02/Source.cpp
@@ -1,5 +1,10 @@
+#include "CWheel.h"
 
 int main() {
+	CWheel *iWheel1 = new CWheel(2, 2, EUNIT_CENTIMETERS);
+	CWheel *iWheel2 = new CWheel(4, 2, EUNIT_CENTIMETERS);
 
+	delete iWheel1;
+	delete iWheel2;
 	return 0;
 }
