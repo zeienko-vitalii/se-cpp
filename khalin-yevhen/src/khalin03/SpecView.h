0c843f74292c9d33431689eb8afa827cdc221057
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-23 18:33:52 +0300

    #1 khalin03: transformed SpecView class to generic type

--- khalin-yevhen/src/khalin03/SpecView.h
+++ khalin-yevhen/src/khalin03/SpecView.h
@@ -2,7 +2,7 @@
 * @file SpecView.h
 * @brief Contains SpecView class declaration
 * @author Khalin Yevhen
-* @version 0.0.1
+* @version 0.0.2
 * @date 22.09.17
 */
 
@@ -10,23 +10,56 @@
 
 #include "BaseView.h"
 
+#include <iostream>
+#include "Utilities.h"
+#include <string>
+using std::string;
+
 /**
 * @brief Prints data source features into an output stream if the source
 * is an instance of the KeyboardButton class, otherwise works with incoming object
 * like ScreenView class.
+* @tparam T necessary type of the data source
+* @author Khalin Yevhen
 */
+template<class T>
 class SpecView : public BaseView
 {
 public:
 
-	/**
-	* @param btn data source
-	* @param out an output stream for printing data source
-	*/
-	SpecView(Button *btn, std::ostream *out);
-	virtual ~SpecView();
+	SpecView(Button * btn, std::ostream *out) : BaseView(btn, out)
+	{
+	}
+
+	virtual ~SpecView()
+	{
+	}
+
 protected:
-	virtual void printHeader();
-	virtual void printContent();
-	virtual void printFooter();
+
+	/**
+	* @TODO it's supposed to implement any behavior
+	*/
+	inline string getSecPrint(Button *btn, string callingMethod) {
+		if (instanceof<T>(btn)) {
+			return callingMethod + "data source is a KeyboardButton instance\n";
+		} else {
+			return callingMethod + "data source is not a KeyboardButton instance\n";
+		}
+	}
+
+	virtual void SpecView::printHeader()
+	{
+		(*getOutputStream()) << getSecPrint(getDataSource(), "SpecView::printHeader():\n\t");
+	}
+
+	virtual void SpecView::printContent()
+	{
+		(*getOutputStream()) << getSecPrint(getDataSource(), "SpecView::printContent():\n\t");
+	}
+
+	virtual void SpecView::printFooter()
+	{
+		(*getOutputStream()) << getSecPrint(getDataSource(), "SpecView::printFooter():\n\t");
+	}
 };
