93de4c6cf9a02cfda12879f60b88e4b0105d354b
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-24 10:44:35 +0300

    #4 khalin03: report is ready to be checked

--- khalin-yevhen/src/khalin03/main.cpp
+++ khalin-yevhen/src/khalin03/main.cpp
@@ -1,12 +1,31 @@
 #include "KeyboardButton.h"
 #include "SpecView.h"
-using namespace std;
 #include <iostream>
 
-void main() {
+void testCastingAndDisplaying() {
+	using std::cout;
+	using std::endl;
+
+	// init Data objects
 	Button btn(ButtonForm::RECTANGULAR);
 	KeyboardButton kb(ButtonForm::RECTANGULAR, 101, "L");
+	Button *btnPtr = &kb; // using a pointer to the base class 
+
+	// init Screen objects 
 	SpecView<KeyboardButton> spec(&btn, &(std::cout));
-	spec.display();
+	BaseView *baseViewPtr = &spec; // using a pointer to the base class
+
+	// displaying data using BaseView pointer
+	cout << "Button instance was sent to SpecView using BaseView pointer:" << endl;
+	baseViewPtr->display(); // Button is supposed to be not a KeyboardButton instance
+
+	// changing data source
+	baseViewPtr->setDataSource(btnPtr);
+	cout << endl << "KeyboardButton instance was sent to SpecView:" << endl;
+	baseViewPtr->display(); // it's supposed to be a KeyboardButton instance
+}
+
+void main() {
+	testCastingAndDisplaying();
 	system("pause");
 }
