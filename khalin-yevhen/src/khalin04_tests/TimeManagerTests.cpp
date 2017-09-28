594ea5acd3b5efb4b7780e0675cde15a46a476f7
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-28 19:22:23 +0300

    #3 khalin04_tests: added checkHandlerCalling test for TimerManagerTests

---
+++ khalin-yevhen/src/khalin04_tests/TimeManagerTests.cpp
@@ -0,0 +1,22 @@
+/**
+* @file TimeManagerTests.cpp
+* @brief Contains TimeManager tests 
+* @author Khalin Yevhen
+* @version 0.0.1
+* @date 28.09.17
+*/
+
+#include <gtest\gtest.h>
+#include "..\khalin04\TimeManager.h"
+#include "..\khalin04\TimeManager.cpp"
+
+TEST(TimeManager, checkHandlerCalling_shouldBeCalled) {
+	static bool handlerWasCalled = false;
+	auto handler = []() { handlerWasCalled = true; };
+	TimeManager tmanager;
+
+	tmanager.setOnTimerAction(handler);
+	tmanager.OnTimerAction(); // handler is supposed to be called
+	
+	ASSERT_TRUE(handlerWasCalled);
+}
