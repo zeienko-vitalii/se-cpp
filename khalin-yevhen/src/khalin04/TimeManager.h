f9f9f00fdcdeceb5cf35bdbc937e4dd302d56b68
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-28 16:16:16 +0300

    #1 khalin04: created TimeManager class

---
+++ khalin-yevhen/src/khalin04/TimeManager.h
@@ -0,0 +1,37 @@
+/**
+* @file TimeManager.h
+* @brief Contains TimeManager class declaration
+* @author Khalin Yevhen
+* @version 0.0.1
+* @date 28.09.17
+*/
+
+#pragma once
+#include <iostream>
+#include <string>
+
+/**
+* @brief Represents a simple event handler
+*/
+class TimeManager {
+public:
+
+	/**
+	* @brief This method is called when event has happened
+	*/
+	void OnTimerAction();
+
+	/**
+	* @brief Sets event handler
+	* @param act handle method 
+	*/
+	void setOnTimerAction(void(*act)());
+
+	/**
+	* @return event handler
+	*/
+	void * getOnTimerAction();
+
+private:
+	void(*action)();
+};
