e062bbaf1a79dae87fce9a34bc22d280ba390163
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-30 18:21:13 +0300

    #1 khalin04: added entry point with task decision demonstration

--- khalin-yevhen/src/khalin04/main.cpp
+++ khalin-yevhen/src/khalin04/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
-#include "TimeManager.h"
-
+#include "SimpleTimer.h"
 #include "KeyboardButton.h"
 #include "..\khalin03\BaseView.cpp"
 #include "SpecView.h"
+#include "SimpleTimer.h"
+#include "SpecViewEventArgs.h"
+
 using namespace std;
 
+inline void showTaskPerformance() {
+	// create data for printing
+	KeyboardButton btn(ButtonForm::RECTANGULAR, 100, "K");
+	SpecView<KeyboardButton> view(&btn, &std::cout);
+	SpecViewEventArgs *args = new SpecViewEventArgs();
+	args->view = &view; // put view into EventArgs
+
+						// create a timer
+	SimpleTimer timer(args, 1000);
+
+	// create an event listener for the timer
+	EventListener l;
+	l.setOnEventAction([](Sender *sender, EventArgs *args) {
+		// we wanna be sure that received necessary args
+		SpecViewEventArgs *ptr = dynamic_cast<SpecViewEventArgs*>(args);
+		if (ptr) {
+			ptr->view->display();
+		}
+	});
+
+	timer.getOnTimerAction()->addListener(&l);
+	timer.start(); // it's supposed to print KeyboardButton data after 1000 ms
+
+	delete args;
+}
+
 void main() {
-	KeyboardButton btn(ButtonForm::RECTANGULAR, 123, "ABZCZZ");
-	SpecView<KeyboardButton> spec(&btn, &cout);
-	spec.setOnTimerAction([]() { cout << "spec::OnTimer" << endl; });
-	spec.OnTimerAction();
-	btn -= 'Z';
-	cout << btn.getName();
+	showTaskPerformance();
 	system("pause");
 }
