ffa50f1d7f585733f95678d9a90cef9f4695a9d2
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-09-30 22:27:42 +0300

    #1 khalin04: added memory leaks detector; code examples for report

--- khalin-yevhen/src/khalin04/main.cpp
+++ khalin-yevhen/src/khalin04/main.cpp
@@ -5,10 +5,36 @@
 #include "SpecView.h"
 #include "SimpleTimer.h"
 #include "SpecViewEventArgs.h"
+#include <vld.h>
 
 using namespace std;
 
-inline void showTaskPerformance() {
+inline void demoOverloadedOperators() {
+	cout << "\ninside demoOverloadedOperators() method:\n";
+	
+	// create button for demo
+	int btnCode = 100;
+	char *btnName = "ABCDB";
+	ButtonForm btnForm = ButtonForm::ROUND;
+	KeyboardButton btn(btnForm, btnCode, btnName);
+	cout << "Button info:\n"
+		<< "\tCode: " << btnCode
+		<< "\n\tName: " << btnName << endl;
+
+	// remove 'B' char from 'ABCDB' button name:
+	const char removingChar = 'B';
+	btn -= removingChar;
+	cout << "After removing " << removingChar << " char button name is: "
+		<< btn.getName() << endl;
+
+	// comare button code with a literal:
+	bool codeCompRes = btn == 100;
+	cout << "Result of code comparison: " << codeCompRes << endl;
+}
+
+inline void demoTimer() {
+	cout << "\ninsode demoTimer() method:" << endl;
+
 	// create data for printing
 	KeyboardButton btn(ButtonForm::RECTANGULAR, 100, "K");
 	SpecView<KeyboardButton> view(&btn, &std::cout);
@@ -22,6 +48,7 @@ inline void showTaskPerformance() {
 		// we wanna be sure that received necessary args
 		SpecViewEventArgs *ptr = dynamic_cast<SpecViewEventArgs*>(args);
 		if (ptr) {
+			cout << "BaseView::display():" << endl;
 			ptr->view->display();
 		}
 	});
@@ -29,6 +56,7 @@ inline void showTaskPerformance() {
 	// create a timer
 	SimpleTimer timer(&e, args, 1000);
 
+	// run timer
 	timer.getOnTimerAction()->addListener(&l);
 	timer.start(); // it's expected to print KeyboardButton data after 1000 ms
 
@@ -36,6 +64,7 @@ inline void showTaskPerformance() {
 }
 
 void main() {
-	showTaskPerformance();
+	demoOverloadedOperators();
+	demoTimer();
 	system("pause");
 }
