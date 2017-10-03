cf1702701f4875b7a867835dccd1bed431b9f429
zeienko-vitalii <31850531+zeienko-vitalii@users.noreply.github.com>
2017-10-03 22:49:04 +0300

    #5 zeienko02: Program was fixed demand to the received comments.

--- zeienko-vitalii/src/zeienko02/main.cpp
+++ zeienko-vitalii/src/zeienko02/main.cpp
@@ -9,45 +9,58 @@
 #include "GraphScreen.h"
 #include "Manipulator.h"
 #include "Screen.h"
+#include <iostream>
 
-/**
- * Entry point of program
- * @return 0 in case if there is no error during runtime.
+#ifdef  _DEBUG
+#define _CRDBG_MAP_ALLOC
+#include <crtdbg.h>
+#define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
+
+ /**
+ * This function contains a basic check for creating
+ * instances of classes and displaying Manipulator`s objects on the screen.
  * */
-int main() {
+void launch() {
 	// ...
-		// Size of the CManipulator's array of objects.
-		const int SIZE = 3;
-		// Creates an instance of CScreen
-		// to call CScreen::Show()
-		Screen* cs[SIZE];
-		GraphScreen* gs[SIZE];
-		// Create 5 objects of type CManipulator
-		// to demonstrate the work of each constructor
-		Manipulator* manip[SIZE];
-		manip[0] = new Manipulator();
-		manip[1] = new Manipulator(4, "Mouse");
-		manip[2] = new Manipulator(*manip[0]);
+	// Size of the CManipulator's array of objects.
+	const int SIZE = 3;
+	// Creates an instance of CScreen
+	// to call CScreen::Show()
+	GraphScreen* gs[SIZE];
+	
+	// Create 3 objects of type CManipulator
+	// to demonstrate the work of each constructor
+	Manipulator* manip[SIZE];
+	manip[0] = new Manipulator();
+	manip[1] = new Manipulator(8, Manipulator::Joystick);
+	manip[2] = new Manipulator(*manip[0]);
 
+	int index = 0; /// temporary variable to access to the object
 
-		int index = 0; /// temporary variable to access to the object
+	 // Output CManipulator's objects
+	for (; index < SIZE; index++) {
+		gs[index] = new GraphScreen(*manip[index]);
+		gs[index]->PrintData();
+	}
 
-		// Output CManipulator's objects
-		for (; index < SIZE; index++) {
-			cs[index] = new Screen(*manip[index]);
-			cs[index]->Show();
-			gs[index] = new GraphScreen(*manip[index]);
-			gs[index]->ShowData();
-		}
-
-		// Deleting created objects to free the memory after allocating
-		for (index = 0; index < SIZE; index++) {
-			delete cs[index];
-			delete gs[index];
-			delete manip[index];
-
-		}
-		// ...
-	return 0;
+	// Deleting created objects to free the memory after allocating
+	for (index = 0; index < SIZE; index++) {
+		delete gs[index];
+		delete manip[index];
+	}
+	// ...
 }
 
+/**
+ * Entry point of program.
+ * @param argc is an amount of parameters which were sent to the main.
+ * @param argv keeps certain arguments which were sent to the main. 
+ * @return 0 in case if there is no error during runtime.
+ * */
+int main(int argc, char* argv) {
+	launch();
+	system("pause");
+	return _CrtDumpMemoryLeaks();
+}
+
+#endif /* _DEBUG*/
