1489fb1c98112721693b794322f7be2f4cff2b47
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-10-13 15:33:04 +0300

    #1 khalin06: add doubly linked list interface

---
+++ khalin-yevhen/src/khalin06/LinkedList.h
@@ -0,0 +1,73 @@
+﻿/**
+* @file LinkedList.h
+* @brief Contains LinkedList class declaration
+* @author Khalin Yevhen
+* @version 0.0.1
+* @date 13.10.17
+*/
+
+#include "..\khalin01\Printable.h"
+#pragma once
+
+/**
+* @brief Abstract doubly linked list interface
+* @author Khalin Yevhen
+*/
+template <typename E>
+class LinkedList : Printable {
+private:
+	template <typename T>
+	class Entry {
+	public:
+		T *element;
+		Entry<T> *next;
+		Entry<T> *prev;
+		Entry(T *element, Entry<T> *next, Entry<T> *prev) :
+			element(element), next(next), prev(prev) { }
+	};
+
+	int elementsAmount;
+	Entry<E> *header; // next = prev
+public:
+	LinkedList() : header(nullptr), elementsAmount(0) {
+	}
+
+	virtual std::string toString() = 0;
+
+	/**
+	* @breif Adds <b>e</b> to the end of the list.
+	*/
+	virtual void addLast(E * e) = 0;
+
+	/**
+	* @breif Adds <b>e</b> to the first position of the list.
+	*/
+	virtual void addFirst(E * e) = 0;
+
+	/**
+	* @breif Removes first element of the list.
+	*/
+	virtual void removeFirst() = 0;
+
+	/**
+	* @breif Removes last element of the list.
+	*/
+	virtual void removeLast() = 0;
+
+	/**
+	* @breif Removes an element by index.
+	*/
+	virtual void remove(unsigned int index) = 0;
+
+	/**
+	* @breif If <b>e</b> is contained in the list, it'll be removed.
+	*/
+	virtual void remove(E * e) = 0;
+
+	/**
+	* @return size of the list
+	*/
+	virtual int size() {
+		return elementsAmount;
+	}
+};
