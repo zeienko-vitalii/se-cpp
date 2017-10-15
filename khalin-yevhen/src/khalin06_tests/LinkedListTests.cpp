94dbff4b66e818a168ab19a0ec13db86b292897b
khalin-yevhen <31850254+khalin-yevhen@users.noreply.github.com>
2017-10-15 22:53:36 +0300

    #3 khalin06_tests: add tests for LinkedList removing methods

--- khalin-yevhen/src/khalin06_tests/LinkedListTests.cpp
+++ khalin-yevhen/src/khalin06_tests/LinkedListTests.cpp
@@ -19,7 +19,7 @@ using namespace std;
 */
 class Dummy { };
 
-TEST(LinkedList, addLast_add3Elements_sizeShouldBeEqualTo3) {
+TEST(LinkedList, addLast_add3Elements_sizeShouldBeEqTo3) {
 	LinkedList<Dummy> list;
 	const int expectedSize = 3;
 
@@ -72,7 +72,6 @@ TEST(LinkedList, addLast_add3Elems_shouldBeAdded—onsistently) {
 	EXPECT_TRUE(firstIsDummy1);
 	EXPECT_TRUE(secondIsDummy2);
 	ASSERT_TRUE(thirdIsDummy3);
-
 	delete dummy1, dummy2, dummy3;
 }
 #endif
@@ -102,12 +101,11 @@ TEST(LinkedList, addFirst_add3Elems_shouldBeAdded—onsistently) {
 	EXPECT_TRUE(firstIsDummy3);
 	EXPECT_TRUE(secondIsDummy2);
 	ASSERT_TRUE(thirdIsDummy1);
-
 	delete dummy1, dummy2, dummy3;
 }
 #endif
 
-TEST(LinkedList, addFirst_add3Elems_sizeShouldBeEqualTo3) {
+TEST(LinkedList, addFirst_add3Elems_sizeShouldBeEqTo3) {
 	LinkedList<Dummy> list;
 	const int expectedSize = 3;
 
@@ -135,7 +133,7 @@ TEST(LinkedList, addFirst_compNextPrevPtrsWith1ElemInList_shouldPointToEachOther
 }
 #endif
 
-TEST(LinkedList, clean_addElemsAndClean_sizeShouldBeEqualTo0) {
+TEST(LinkedList, clean_addElemsAndClean_sizeShouldBeEqTo0) {
 	LinkedList<Dummy> list;
 	const int expectedSize = 0;
 
@@ -152,3 +150,167 @@ TEST(LinkedList, clean_addElemsAndClean_sizeShouldBeEqualTo0) {
 	ASSERT_EQ(expectedSize, actualSize);
 }
 
+TEST(LinkedList, removeFirst_removeFromListWith3Elem_sizeShouldBeEqTo2) {
+	LinkedList<Dummy> list;
+	const int expectedSize = 2;
+
+	// act: add dummy objects
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.removeFirst();
+
+	const int actualSize = list.size();
+
+	ASSERT_EQ(expectedSize, actualSize);
+}
+
+#ifdef TEST_MODE
+TEST(LinkedList, removeFirst_removeFromListWith2Elem_headShouldPointToSecElem) {
+	LinkedList<Dummy> list;
+	FieldsAccessor<Dummy> accessor(&list);
+	Dummy *firstDummy = new Dummy();
+	Dummy *secDummy = new Dummy();
+	const auto expectedElement = secDummy;
+
+	// act:
+	list.addFirst(secDummy);
+	list.addFirst(firstDummy);
+	list.removeFirst();
+	const auto actualElement = accessor.getHead()->element;
+
+	ASSERT_EQ(expectedElement, actualElement);
+	delete firstDummy, secDummy;
+}
+#endif
+
+#ifdef TEST_MODE
+TEST(LinkedList, removeFirst_removeFromListWith2Elem_headAndTailShouldPointToEachOther) {
+	LinkedList<Dummy> list;
+	FieldsAccessor<Dummy> accessor(&list);
+
+	// act:
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.removeFirst();
+
+	auto head = accessor.getHead();
+	auto tail = accessor.getTail();
+
+	ASSERT_EQ(head, tail);
+}
+#endif
+
+TEST(LinkedList, removeLast_removeFromListWith3Elem_sizeShouldBeEqTo2) {
+	LinkedList<Dummy> list;
+	const int expectedSize = 2;
+
+	// act: add dummy objects
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.removeLast();
+
+	const int actualSize = list.size();
+
+	ASSERT_EQ(expectedSize, actualSize);
+}
+
+#ifdef TEST_MODE
+TEST(LinkedList, removeLast_removeFromListWith2Elem_tailShouldPointToFirstElem) {
+	LinkedList<Dummy> list;
+	FieldsAccessor<Dummy> accessor(&list);
+	Dummy *firstDummy = new Dummy();
+	Dummy *secDummy = new Dummy();
+	const auto expectedElement = firstDummy;
+
+	// act:
+	list.addFirst(secDummy);
+	list.addFirst(firstDummy);
+	list.removeLast();
+	const auto actualElement = accessor.getHead()->element;
+
+	ASSERT_EQ(expectedElement, actualElement);
+	delete firstDummy, secDummy;
+}
+#endif
+
+// just checking list size after calling remove(int) method.
+TEST(LinkedList, removeByIndex_removeFirstFromListWith3Elems_sizeShouldBeEqTo2) {
+	LinkedList<Dummy> list;
+	const int expectedSize = 2;
+	auto removingIndex = 0;
+
+	// act: add dummy objects
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.addLast(&Dummy());
+	list.remove(removingIndex);
+
+	const int actualSize = list.size();
+
+	ASSERT_EQ(expectedSize, actualSize);
+}
+
+#ifdef TEST_MODE
+TEST(LinkedList, removeByIndex_removeFirstElem_headShouldPointToSecond) {
+	LinkedList<Dummy> list;
+	FieldsAccessor<Dummy> accessor(&list);
+	Dummy *firstDummy = new Dummy();
+	Dummy *secDummy = new Dummy();
+	const auto expectedElement = secDummy;
+	int removingIndex = 0;
+
+	// act:
+	list.addLast(firstDummy);
+	list.addLast(secDummy);
+	list.remove(removingIndex);
+	auto actualElement = accessor.getTail()->element;
+
+	ASSERT_EQ(expectedElement, actualElement);
+	delete firstDummy, secDummy;
+}
+#endif
+
+#ifdef TEST_MODE
+TEST(LinkedList, removeByIndex_removeLast_tailShouldBeEqToFirst) {
+	LinkedList<Dummy> list;
+	FieldsAccessor<Dummy> accessor(&list);
+	Dummy *firstDummy = new Dummy();
+	Dummy *secDummy = new Dummy();
+
+	const auto expectedElement = firstDummy;
+	int removingIndex = 1;
+
+	// act:
+	list.addLast(firstDummy);
+	list.addLast(secDummy);
+
+	list.remove(removingIndex);
+	auto actualElement = accessor.getTail()->element;
+
+	ASSERT_EQ(expectedElement, actualElement);
+	delete firstDummy, secDummy;
+}
+#endif
+
+#ifdef TEST_MODE
+TEST(LinkedList, removeByValue_removeSecond_tailShouldBeEqToFirst) {
+	LinkedList<Dummy> list;
+	FieldsAccessor<Dummy> accessor(&list);
+	Dummy *firstDummy = new Dummy();
+	Dummy *secDummy = new Dummy();
+
+	const auto expectedElement = firstDummy;
+	int removingIndex = 1;
+
+	// act:
+	list.addLast(firstDummy);
+	list.addLast(secDummy);
+	list.remove(secDummy);
+	auto actualElement = accessor.getTail()->element;
+
+	ASSERT_EQ(expectedElement, actualElement);
+	delete firstDummy, secDummy;
+}
+#endif
